d216a15d2eaf16d47f4f58f85a82a722ca470274
Phúc Anh <anhle7666@gmail.com>
2023-11-24 16:44:03 +0700

    s

--- src/db.js
+++ src/db.js
@@ -1,18 +1,18 @@
 // db.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'LocalDB',
+    host: "sql12.freesqldatabase.com",
+    user: "sql12664454",
+    password: "WtwjKLAEJP",
+    database: "sql12664454",
 });
 
 connection.connect((err) => {
     if (err) {
-        console.error('Lỗi kết nối đến cơ sở dữ liệu:', err);
+        console.error("Lỗi kết nối đến cơ sở dữ liệu:", err);
     } else {
-        console.log('Kết nối đến cơ sở dữ liệu thành công');
+        console.log("Kết nối đến cơ sở dữ liệu thành công");
     }
 });
 
